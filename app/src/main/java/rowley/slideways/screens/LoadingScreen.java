de93e1b120f4a3da1f9c699c05a929dfa24bc745
Joe Rowley <jrowley@vertigo.com>
2015-11-03 11:08:14 -0700

    Loading up the word trie for use in the app

--- app/src/main/java/rowley/slideways/screens/LoadingScreen.java
+++ app/src/main/java/rowley/slideways/screens/LoadingScreen.java
@@ -1,15 +1,28 @@
 package rowley.slideways.screens;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.util.Log;
 
 import jrowley.gamecontrollib.game_control.GameController;
 import jrowley.gamecontrollib.screen_control.ScreenController;
+import rowley.slideways.R;
+import rowley.slideways.util.Assets;
+import rowley.slideways.util.FrameRateTracker;
+import rowley.wordtrie.WordTrie;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by jrowley on 11/2/15.
  */
 public class LoadingScreen extends ScreenController {
+    private final String TAG = "LoadingScreen";
+
     private float percentComplete = 0f;
     private int maxWidth = 200;
     private int barHeight = 30;
@@ -20,12 +33,38 @@ public class LoadingScreen extends ScreenController {
     private int barLeft;
     private int barTop;
     private final int SECONDS_TO_LOAD = 5;
+    private FrameRateTracker frameRateTracker;
+
+    private volatile boolean loadComplete = false;
 
     public LoadingScreen(GameController gameController) {
         super(gameController);
 
         barLeft = centerWidth - (maxWidth / 2);
         barTop = centerHeight - 15;
+
+        frameRateTracker = new FrameRateTracker();
+
+        Observable.create(new Observable.OnSubscribe<Void>() {
+            @Override
+            public void call(Subscriber<? super Void> subscriber) {
+                Assets.wordTrie = new WordTrie.WordTrieBuilder().addSowpodWords(Assets.MAX_WORD_LENGTH).addEnableWords(Assets.MAX_WORD_LENGTH).build();
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.newThread()).observeOn(Schedulers.immediate()).subscribe(new Action1<Void>() {
+            @Override
+            public void call(Void aVoid) {
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+            }
+        }, new Action0() {
+            @Override
+            public void call() {
+                loadComplete = true;
+            }
+        });
     }
 
     @Override
@@ -37,13 +76,18 @@ public class LoadingScreen extends ScreenController {
         if (percentComplete > 1) {
             percentComplete = 0;
         }
-        Log.d("LoadingScreen", "portionOfSecond: " + portionOfSecond + " progress: " + progress + " PercentComplete: " + percentComplete);
+        frameRateTracker.update(portionOfSecond);
     }
 
     @Override
     public void present(float portionOfSecond) {
-        gameController.getGraphics().drawRect(0, 0, screenWidth, screenHeight, Color.BLUE);
-        gameController.getGraphics().drawRect(barLeft, barTop, (int)(maxWidth * percentComplete), barHeight, Color.GREEN);
+        gameController.getGraphics().clear(Color.BLUE);
+        gameController.getGraphics().drawRect(barLeft, barTop, (int) (maxWidth * percentComplete), barHeight, Color.GREEN);
+        gameController.getGraphics().writeText(gameController.getStringResource(R.string.loading), centerWidth, centerHeight + barHeight, Color.WHITE, 40f, Typeface.SANS_SERIF);
+        gameController.getGraphics().writeText(String.valueOf(frameRateTracker.getFrameRate()) + " fps", 100, 25, Color.WHITE, 10f, Typeface.SANS_SERIF);
+        if(loadComplete) {
+            gameController.getGraphics().writeText(Assets.wordTrie.getWordCount() + " words loaded", centerWidth, centerHeight + barHeight + 45, Color.WHITE, 25f, Typeface.DEFAULT_BOLD);
+        }
     }
 
     @Override
---
+++ app/src/main/java/rowley/slideways/util/Assets.java
@@ -0,0 +1,11 @@
+package rowley.slideways.util;
+
+import rowley.wordtrie.WordTrie;
+
+/**
+ * Created by jrowley on 11/3/15.
+ */
+public class Assets {
+    public static final int MAX_WORD_LENGTH = 9;
+    public static WordTrie wordTrie;
+}
